65e6959e542efe7e8b8a6379aaa439090004dce0
angel651 <74489408+angel651@users.noreply.github.com>
2020-11-30 15:28:33 +0800

    Springboot+Websocket+MYSQL搭建的聊天软件

--- src/main/java/com/Xiaoyi/ServletInitializer.java
+++ src/main/java/com/Xiaoyi/ServletInitializer.java
@@ -3,6 +3,9 @@ package com.Xiaoyi;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * Author Xiaoyi
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
